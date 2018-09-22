c70faf1373785d95b122148ecbec470ccab910a1
shadowlaq <escamillaa@email.arizona.edu>
2018-09-22 11:14:38 -0700

    first heroku push

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require("./routes");
+
 
 
 const models = require('./models');
@@ -10,6 +12,20 @@ const models = require('./models');
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Use bodyParser in our app
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Have every request go through our route middleware
+app.use(routes);
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
+
 
 // Send every request to the React app
 // Define any API routes before this runs
