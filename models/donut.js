5406858bb5bc2aa52d0b3df73e77c414d308da7c
imxmolina <34321601+imxmolina@users.noreply.github.com>
2018-09-22 12:03:13 -0700

    Update donut.js

--- models/donut.js
+++ models/donut.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const donutSchema = new Schema({
+    id: {type: Schema.Types.ObjectId, required: true },
     name: { type: String, required: true },
     image: { type: String, required: true }
 });
