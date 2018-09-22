48ce17246369f21748fa1658166f766390cbb8a9
imxmolina <34321601+imxmolina@users.noreply.github.com>
2018-09-22 12:03:56 -0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,10 +3,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    uid: { type: String, required: true },
-    fname: { type: String, required: true },
-    lname: { type: String, required: true },
+    id: { type: Schema.Types.ObjectId, required: true },
     username: { type: String, required: true },
+    email: {type: String, required: true},
     password: { type: String, required: true }
 });
 
