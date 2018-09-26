d4ec27421279dcfca33f361fd2e817658b0876e9
IMXMOLINA <imxmolina@gmail.com>
2018-09-26 14:45:22 -0700

    buttonclicks work

--- client/src/pages/Order/order.js
+++ client/src/pages/Order/order.js
@@ -6,8 +6,10 @@ class Order extends Component {
     state = {
         donuts: [],
         name: "",
+        id:"",
         image: "",
         users: [],
+        box: []
     };
 
     componentDidMount() {
@@ -18,22 +20,29 @@ class Order extends Component {
         //API FOR GET DONUT
         API.getDonuts()
             .then(res =>
-                this.setState({ donuts: res.data, name: "" })
+                this.setState({ donuts: res.data, name: "", id: ""})
             )
             .catch(err => console.log(err));
     };
 
+    handleClick = (id) => {
+        console.log(id)
+    }
+
     render() {
+        console.log(this.state.donuts);
+        
         return (
             <div>
                 <h1>CHOICES</h1>
                 {this.state.donuts.length ? (
                     <DonutChoice>
                         {this.state.donuts.map(Donut => (
-                            <DonutItem key={Donut._id}>
-                               <div> <li>   
-                                <img src={Donut.image} alt={Donut.name} width="50" height="50"/>
-                                {Donut.name}
+                            <DonutItem key={Donut._id} handleClick={this.handleClick} donut_id={Donut._id}>
+                                <div> 
+                                <li>
+                                    <img src={Donut.image} alt={Donut.name} width="50" height="50" />
+                                    {Donut.name}
                                 </li>
                                 </div>
                             </DonutItem>
