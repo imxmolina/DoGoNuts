fff8bdae45cd37a74be9a1fe862fe5730529816c
AZyrek24 <zzeppelin2400@gmail.com>
2018-09-27 20:39:01 -0700

    added deleteBtn

--- controllers/boxControllers.js
+++ controllers/boxControllers.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   update: function(req, res) {
     db.Box
-      .findOneAndUpdate({ _id: "5bac50a33581942e3d5e08a0" }, { $push: { donutcount: req.body.id }})
+      .findOneAndUpdate({ _id: "5bad9eb97dd71745d285f39b" }, { $push: { donutcount: req.body.id }})
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
