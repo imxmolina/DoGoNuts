cb04001cd76f2ccd462f114b35a2405c30c5c463
AZyrek24 <zzeppelin2400@gmail.com>
2018-09-30 07:52:44 -0700

    put everything back, switching to new branch

--- client/src/components/CreateBox/CreateBox.js
+++ client/src/components/CreateBox/CreateBox.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./CreateBox.css";
 
 export const CreateBox = ({ children }) => {
+  console.log(children);
   return (
     <div>
       <div id="create-box-wrapper">
