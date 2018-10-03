a605ead62f5640f5142d472d0b9dda272f71eea8
AZyrek24 <zzeppelin2400@gmail.com>
2018-10-03 12:07:10 -0700

    setting up passport

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes/api");
 const app = express();
+const passport = require("passport");
 const PORT = process.env.PORT || 3001;
 
 // Configure body parser for AJAX requests
@@ -13,6 +14,14 @@ app.use(bodyParser.json());
 app.use(express.static("client/build"));
 // Add routes, both API and view
 app.use(routes);
+
+app.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/users/' + req.user.username);
+  });
 
 // Set up promises with mongoose
 mongoose.Promise = global.Promise;
