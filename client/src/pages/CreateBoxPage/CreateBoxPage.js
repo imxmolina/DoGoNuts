6c2777720f2cc7f485bfbd20d16c7d5e11a011c0
AZyrek24 <zzeppelin2400@gmail.com>
2018-10-07 10:57:25 -0700

    createBoxPage now protected and redirects to login, logout button redirects to login

--- client/src/pages/CreateBoxPage/CreateBoxPage.js
+++ client/src/pages/CreateBoxPage/CreateBoxPage.js
@@ -2,12 +2,23 @@ import React, { Component } from "react";
 import { CreateBox } from "../../components/CreateBox";
 import API from "../../utils/API";
 // import ReactModal from 'react-modal';
+import axios from "axios";
 
 class CreateBoxPage extends Component {
     state = {
         box: [],
         boxname: "",
         boxId: ""
+    }
+
+    checkIfAuth = () => {
+        axios.defaults.headers.common['Authorization'] = localStorage.getItem('jwtToken');
+        API.checkAuth()
+            .catch(err => this.props.history.push("/login"));
+    }
+
+    componentDidMount() {
+        this.checkIfAuth();
     }
     handleCreateBox = name => {
         const boxData = { boxname: name }
@@ -28,6 +39,7 @@ class CreateBoxPage extends Component {
                 <CreateBox boxname={this.state.boxname} handleCreateBox={this.handleCreateBox} />
                 <p>http://localhost:3000/api/box/{this.state.boxId}</p>
             </div>
+            
         )
     }
 }
